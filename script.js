a81d0437d009b58dc50cab69f45b347854f2a48e
brunoBMonteiro <brunobilheri@hotmail.com>
2021-02-01 22:19:35 -0300

    procurar o erro no date

--- script.js
+++ script.js
@@ -11,38 +11,47 @@ const Modal = {
     }
 }
 
-const transactions = [
-    {
-        id: 1,
-        description: 'Luz',
-        amount: -50000,
-        date: '30/01/2021'
-    }, 
-    {
-        id: 2,
-        description: 'Website',
-        amount: 500000,
-        date: '30/01/2021'
-    }, 
-    {
-        id: 3,
-        description: 'Agua',
-        amount: -20000,
-        date: '30/01/2021' 
-    },
-    {
-        id: 4,
-        description: 'App',
-        amount: 20000,
-        date: '30/01/2021' 
-    }
-]
-
 const Transaction = {
+    all: [
+        {
+            description: 'Luz',
+            amount: -50000,
+            date: '30/01/2021'
+        }, 
+        {
+            description: 'Website',
+            amount: 500000,
+            date: '30/01/2021'
+        }, 
+        {
+            description: 'Agua',
+            amount: -20000,
+            date: '30/01/2021' 
+        },
+        {
+            description: 'App',
+            amount: 20000,
+            date: '30/01/2021' 
+        }
+    ],
+
+    add(transaction){
+        Transaction.all.push(transaction)
+    
+        App.reload()
+    
+    },
+
+    remove(index) {
+        Transaction.all.splice(index, 1)
+
+        App.reload()
+    },
+
     incomes() {
         let income = 0;
 
-        transactions.forEach(transaction => {
+        Transaction.all.forEach(transaction => {
             if(transaction.amount > 0) {
                 income += transaction.amount;
             }
@@ -53,7 +62,7 @@ const Transaction = {
     expenses() {
             let expense = 0;
     
-            transactions.forEach(transaction => {
+            Transaction.all.forEach(transaction => {
                 if(transaction.amount < 0) {
                     expense += transaction.amount;
                 }
@@ -105,10 +114,25 @@ const DOM = {
         document
             .getElementById('totalDisplay')
             .innerHTML = Utils.formatCurrency(Transaction.total())
+    },
+
+    clearTransactions() {
+        DOM.transactionsContainer.innerHTML = ""
     }
 }
 
 const Utils = {
+    formatAmount(value) {
+        value = Number(value) * 100
+
+        return value
+    },
+
+    formatDate(date) {
+        const splittedDate = date.split("-")
+        return `${splittedDate[2]}/${splittedDate[1]}/${splittedDate[0]}`
+    },
+
     formatCurrency(value) {
         const signal = Number(value) < 0 ? "-" : ""
 
@@ -125,8 +149,77 @@ const Utils = {
     }
 }
 
-transactions.forEach(function(transaction) {
-    DOM.addTransaction(transaction)
-})
+const Form = {
+    description: document.querySelector('input#description'),
+    description: document.querySelector('input#amount'),
+    description: document.querySelector('input#date'),
 
-DOM.updateBalance()
+    getValues() {
+        return {
+            description: Form.description.value,
+            amount: Form.amount.value,
+            date: Form.date.value
+        }
+    },
+
+    validateFields() {
+        const { description, amount, date} = Form.getValues()
+
+        if(description.trim() === "" || amount.trim() === "" || date.trim() === "") {
+            throw new Error("Por favor, preencha todos os campos")
+        }
+    },
+
+    formatValues() {
+        let { description, amount, date} = Form.getValues()
+
+        amount = Utils.formatAmount(amount)
+
+        date = Utils.formatDate(date)
+
+        return {
+            description,
+            amount,
+            date
+        }
+    },
+
+    submit(event) {
+        event.preventDefault()
+
+        try {
+            //Form.validateFields()
+
+            const transaction = Form.formatValues()
+        } catch (error) {
+            alert(error.message)
+        }
+
+        
+
+        
+    }
+}
+
+const App = {
+    init() {
+
+        Transaction.all.forEach(transaction => {
+            DOM.addTransaction(transaction)
+        })
+
+
+
+        DOM.updateBalance()
+
+    },
+    reload() {
+        DOM.clearTransactions()
+        App.init()
+    },
+}
+
+App.init()
+
+//Transaction.remove(0)
+
